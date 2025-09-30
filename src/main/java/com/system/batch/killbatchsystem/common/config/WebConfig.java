4a0c57a2e375c72d3fda07f9577c5dce6dad6f93
normaldeve <junnukim1007@gmail.com>
2025-09-30 18:18:12 +0900

    DTO가 아닌 응답 값도 공통 응답 처리 될 수 있도록 MessageConverter 커스텀 이후 가장 높은 우선 순위 설정

--- src/main/java/com/system/batch/killbatchsystem/common/config/WebConfig.java
+++ src/main/java/com/system/batch/killbatchsystem/common/config/WebConfig.java
@@ -1,12 +1,20 @@
 package com.system.batch.killbatchsystem.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.system.batch.killbatchsystem.common.api.CustomHttpMessageConverter;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+  private final ObjectMapper objectMapper;
 
   @Value("${cors.allowed-origins}")
   private String allowedOrigins;
@@ -18,5 +26,10 @@ public class WebConfig implements WebMvcConfigurer {
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
         .allowedHeaders("*")
         .allowCredentials(true);
+  }
+
+  @Override
+  public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    converters.add(0, new CustomHttpMessageConverter(objectMapper));
   }
 }
